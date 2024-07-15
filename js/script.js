2889ca4f4ce43bfa4ff5c269c83f20a83bf0aa6b
eltonanit <elton.ani@gmail.com>
2024-07-15 19:09:18 +0200

    [UPD] Added function for timer 30 second

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ document.getElementById('gioca').addEventListener('click', function () {
     const result = document.getElementById('result');
     result.innerHTML = '';
 
+// Genero 5 numeri casuali
+const numerirandom = generatenumerirandom(5, 1, 100);
+numeri.textContent = numerirandom.join(', ');
+
+// Funzione timer di 30 secondi
+setTimeout(function () {
+      numeri.textContent = '';
+
+    // Chiedo all'utente di inserire i numeri
+    const user = [];   
+    for (let i = 0; i < 5; i++) {
+            const userInput = parseInt(prompt('Inserisci un numero che hai visto:'));
+            if (!isNaN(userInput)) {
+                user.push(userInput);
+            }
+        }
+
+        // Verifichiamo quanti e quali numeri sono stati individuati
+        const numerogiusto = numerirandom.filter(num => user.includes(num));
+        result.innerHTML = `
+            <h3>Risultato:</h3>
+            <p>Hai indovinato ${numerogiusto.length} numeri su 5.</p>
+            <p>Numeri corretti: ${numerogiusto.join(', ')}</p>
+        `;
+
+    }, 30000);
+});
