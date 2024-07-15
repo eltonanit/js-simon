539a188d79fe2cbfc01cad2bd209f79098f4309f
eltonanit <elton.ani@gmail.com>
2024-07-15 19:05:18 +0200

    [UPD] Added file js ; Recupera ID numbers from Dom

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+// Recupero i numeri e il risultato dal dom 
+document.getElementById('gioca').addEventListener('click', function () {
+    const numeri = document.getElementById('numeri');
+    const result = document.getElementById('result');
+    result.innerHTML = '';
+
